b03df8630bf37012edcd1af4700975e9c2469ba4
sliucw <sliucw@gmail.com>
2020-08-10 15:48:04 +0800

    fix: Defaults.statusBluetooth crash

--- Source/Supporting Files/Utilities.swift
+++ Source/Supporting Files/Utilities.swift
@@ -82,7 +82,7 @@ struct Defaults {
     static var requestedBluetooth: Bool
 
     @UserDefault("permission.statusBluetooth", defaultValue: nil)
-    static var statusBluetooth: PermissionStatus?
+    static var statusBluetooth: PermissionStatus.RawValue?
 
     @UserDefault("permission.stateBluetoothManagerDetermined", defaultValue: false)
     static var stateBluetoothManagerDetermined: Bool
